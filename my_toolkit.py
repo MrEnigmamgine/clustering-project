dbc05612ed0a452c1ad09896fbcd223a3cb2b331
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2022-08-05 18:10:56 -0400

    save

--- my_toolkit.py
+++ my_toolkit.py
@@ -371,3 +371,74 @@ def drop_upper_and_lower_outliers(df, cols, k=1.5):
         df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
     return df
 
+#####################################################
+#                  DATA EXPLORATION                 #
+#####################################################
+
+def anova_variance_in_target_for_cat(df, target, cat, alpha=0.5):
+    from scipy.stats import f_oneway
+    s= df[cat]
+    vals = s.sort_values().unique()
+    subsets = [df[s == vals[x]][target] for x, v in enumerate(vals)]
+    stat, p = f_oneway(*subsets)
+    result={'reject': p < alpha,
+            'h0' : f"There is no variance in {target} between subsets of {cat}",
+            'stat_name': 'F',
+            'stat': stat
+        }
+    return result
+
+
+#####################################################
+#                DATA MODEL PREPPING                #
+#####################################################
+
+
+
+
+def find_k(df, cluster_vars, k_range):
+    from sklearn.cluster import KMeans
+    sse = []
+    for k in k_range:
+        kmeans = KMeans(n_clusters=k)
+
+        # X[0] is our df dataframe..the first dataframe in the list of dataframes stored in X. 
+        kmeans.fit(df[cluster_vars])
+
+        # inertia: Sum of squared distances of samples to their closest cluster center.
+        sse.append(kmeans.inertia_) 
+
+    # compute the difference from one k to the next
+    delta = [round(sse[i] - sse[i+1],0) for i in range(len(sse)-1)]
+
+    # compute the percent difference from one k to the next
+    pct_delta = [round(((sse[i] - sse[i+1])/sse[i])*100, 1) for i in range(len(sse)-1)]
+
+    # create a dataframe with all of our metrics to compare them across values of k: SSE, delta, pct_delta
+    k_comparisons_df = pd.DataFrame(dict(k=k_range[0:-1], 
+                             sse=sse[0:-1], 
+                             delta=delta, 
+                             pct_delta=pct_delta))
+
+    # plot k with inertia
+    plt.plot(k_comparisons_df.k, k_comparisons_df.sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('SSE')
+    plt.title('The Elbow Method to find the optimal k\nFor which k values do we see large decreases in SSE?')
+    plt.show()
+
+    # plot k with pct_delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.pct_delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Percent Change')
+    plt.title('For which k values are we seeing increased changes (%) in SSE?')
+    plt.show()
+
+    # plot k with delta
+    plt.plot(k_comparisons_df.k, k_comparisons_df.delta, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Absolute Change in SSE')
+    plt.title('For which k values are we seeing increased changes (absolute) in SSE?')
+    plt.show()
+
+    return k_comparisons_df
